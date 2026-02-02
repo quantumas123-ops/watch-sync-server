c4317544d9c904a2c5aff5cdc05ad8f06031be5d
quantumas123-ops <quantumas123@gmail.com>
2026-02-02 20:35:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
-const io = require("socket.io")(process.env.PORT || 3000, {
-  cors: { origin: "*" }
+const http = require("http");
+const { Server } = require("socket.io");
+
+// Create HTTP server
+const server = http.createServer();
+
+// Attach Socket.IO to HTTP server
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
 io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
   socket.on("sync", (data) => {
     socket.broadcast.emit("sync", data);
   });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
 });
+
+// IMPORTANT: listen on Render PORT
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
+
